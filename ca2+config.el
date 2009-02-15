0cc25e13488f93ecb0b644b828f8ffde5de9245a
hannes <jeff@x40.(none)>
2009-02-16 00:11:30 +0100

    start global-mode

--- ca2+config.el
+++ ca2+config.el
@@ -3,7 +3,6 @@
 ;;
 ;; sources are pushed, so load lower priority sources first
 
-
 (ca-clear-completion-sources)
 
 
@@ -14,6 +13,7 @@
 (ca-add-completion-source ca-dabbrev-source
 			  'otherwise)
 
+;; mode sources
 (ca-add-completion-source ca-gtags-source
 			  '(c++-mode c-mode java-mode))
 
@@ -26,6 +26,6 @@
 			    lisp-interaction-mode
 			    c++-mode c-mode java-mode))
 
-
+(global-ca-mode 1)
 
 (provide 'ca2+config)
