52f4c8f5f4daa45861a32ae057a02f61934fcc3e
hannes <jeff@x40.(none)>
2009-02-22 14:15:24 +0100

    keep yasnippet from doing endless recursions

--- ca2+config.el
+++ ca2+config.el
@@ -1,6 +1,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; install sources ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 (ca-clear-completion-sources)
 
 ;; mode sources
@@ -14,11 +15,25 @@
 			  '(emacs-lisp-mode 
 			    lisp-interaction-mode))
 
-;; TODO how to get modes from yasnippet?
-(ca-add-completion-source ca-source-yasnippet
-			  '(emacs-lisp-mode 
-			    lisp-interaction-mode
-			    c++-mode c-mode java-mode))
+
+(eval-after-load 'yasnippet
+  '(progn
+     ;; disable yas/trigger key - we call you!!!
+     (setq yas/trigger-key (kbd "C-c <kp-multiply>"))
+     ;; change this to your liking, but it tab would interfer 
+     ;; with completion within yas templates.
+     (setq yas/next-field-key (kbd "C-f"))
+     (setq yas/prev-field-key (kbd "C-b"))
+
+     ;; expand only if word matches template name,
+     ;; otherwise ca2+ shows possible completions
+     (setq yas/fallback-behaviour 'return-nil)
+
+     (ca-add-completion-source ca-source-yasnippet
+			       '(emacs-lisp-mode 
+				 lisp-interaction-mode
+				 c++-mode c-mode java-mode 
+				 'otherwise))))
 
 (eval-after-load 'semantic
   '(progn 
@@ -26,7 +41,7 @@
      ;; experimental, but faster version. 
      ;; you need to run ca-semantic-clear-cache 
      ;; to update cached tags tables
-     ;;(require 'ca2+semantic)
+     (require 'ca2+semantic)
      (ca-add-completion-source ca-source-semantic-context
 			       '(c++-mode c-mode java-mode))
      (ca-add-completion-source ca-source-semantic-tags
@@ -46,6 +61,7 @@
 
 ;; tab expands current candidate
 ;;(define-key ca-active-map [tab] 'ca-expand-top)
+
 
 (global-ca-mode 1)
 
