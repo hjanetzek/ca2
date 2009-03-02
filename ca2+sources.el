41c709738f53db6be18844b066f5f517d0491134
hannes <jeff@x40.(none)>
2009-03-02 02:18:43 +0100

    - semantic function completion action now inserts the local variables that most likely would be given as arguments. - cleaned up semantic action -> changed to 'continue' actions - merged semantic tag-argument and context sources

--- ca2+sources.el
+++ ca2+sources.el
@@ -85,16 +85,23 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; filename ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; added '+'
+(setq thing-at-point-file-name-chars "-~/[:alnum:]_.${}#+%,:")
 
 (defun ca-source-filename-candidates (prefix)
+  (let (strip-dash)
+  (unless (string-match "\\(\./\\|\.\./\\|~/\\)" prefix)
+    (setq strip-dash t)
+    (setq prefix (concat "./" prefix)))
+  ;;(message "prefix %s" prefix)
   (let ((dir (file-name-directory prefix)))
     (ignore-errors
-      (mapcar (lambda (file) (concat dir file))
+      (mapcar (lambda (file) 
+		(concat (if strip-dash (substring dir 2) dir) file))
               (remove-if (lambda (file) (or (equal file "../")
                                             (equal file "./")))
                          (file-name-all-completions
-                          (file-name-nondirectory prefix) dir))))))
-
+                          (file-name-nondirectory prefix) dir)))))))
 
 (defvar ca-source-filename
   '((candidates . ca-source-filename-candidates)
@@ -117,7 +124,7 @@
    '(lambda (cand) 
       (or (boundp (intern cand))
 	  (fboundp (intern cand))))
-   (all-completions prefix obarray)))
+   (all-completions (regexp-quote prefix) obarray)))
 
 
 (defun ca-source-lisp-describe (candidate)
@@ -129,7 +136,7 @@
 	(t
 	 (message "no description")))))
 
-
+ 
 ;;(defun ca-source-lisp-action (candidate))
 
 (defvar ca-source-lisp
@@ -205,6 +212,7 @@
 
 (defvar ca-source-yasnippet
   '((candidates . ca-source-yasnippet-candidates)
+    (decider    . word)
     (action     . (lambda(candidate) (yas/expand)))
     (limit      . 1)
     (sorted     . t)
@@ -214,7 +222,219 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; semantic ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; sematinc tags completion
+
+(defun ca-source-semantic-find-local-tag (tag)
+  (let* ((p (point))
+	 (a (or ca-source-semantic-context-cntxt
+		(semantic-analyze-current-context p)))
+	 (ca-source-semantic-context-cntxt a)
+	 (scope (oref a scope))
+	 (localvar (oref scope localvar))
+	 (type (semantic-tag-type tag))
+	 (name (semantic-tag-name tag))
+	 (tmp (nconc
+	       ;; Argument list and local variables
+	       (semantic-find-tags-by-type type localvar)
+	       ;; The current scope
+	       (semantic-find-tags-by-type type (oref scope fullscope))))
+	 localtags)
+
+    ;; filter out local vars that were defined after point
+    (dolist (ptag tmp)
+      (if (< (aref (car (reverse ptag)) 1) p)
+	  (push ptag localtags)))
+    (when localtags
+      (or
+       (find-if '(lambda (item) 
+		   (string-equal name (semantic-tag-name item))) 
+		localtags)
+       (find-if '(lambda (item) 
+		   (string-match name (semantic-tag-name item))) 
+		localtags)
+       (find-if '(lambda (item) 
+		   (string-match (semantic-tag-name item) name)) 
+		localtags)
+       (car localtags)))))
+
+
+;; modified from semantic-format.el, creates yas-template
+(defun ca-source-semantic-format-tag-arguments (args formatter)
+  "Format the argument list ARGS with FORMATTER.
+FORMATTER is a function used to format a tag.
+COLOR specifies if color should be used."
+  (let ((out nil)
+	(cnt 1))
+    (while args
+      (unless (equal (semantic-tag-name (car args)) "")
+	  (push 
+	   (concat "${" (number-to-string cnt) ":"
+		   (or
+		    ;; try find matchin candidate
+		    (and (semantic-tag-p (car args))
+			 (let* ((tag (car args))
+				(ptag (ca-source-semantic-find-local-tag tag)))
+			   (if ptag
+			       (concat
+				(or (and (semantic-tag-get-attribute tag :pointer)
+					 (not (semantic-tag-get-attribute ptag :pointer))
+					 "&")
+				    "")
+				(semantic-tag-name ptag)))))
+		    ;; try tag formatter
+		    (and formatter
+			 (semantic-tag-p (car args))
+			 (not (string= (semantic-tag-name (car args)) ""))
+			 (funcall formatter (car args) nil nil))
+		    ;; fallback
+		    (semantic-format-tag-name-from-anything
+		     (car args) nil nil 'variable))
+		   "}")
+		out))
+      (setq cnt (1+ cnt))
+      (setq args (cdr args)))
+    ;;semantic-function-argument-separator
+    (mapconcat 'identity (nreverse out) ", ")))
+
+
+(defun ca-source-semantic-continue-function (tag)
+  ;;(message "complete function: %s" tag)
+  (yas/expand-snippet 
+   (point) (point) 
+   (concat 
+    "(" (ca-source-semantic-format-tag-arguments
+	 (semantic-tag-function-arguments tag)
+	 #'semantic-format-tag-prototype) ")"
+	 (unless(looking-at "[[:space:]]*\\()\\|,\\)") ";")))
+  ;; finish ca2+
+  nil)
+
+
+(defun ca-source-semantic-continue-variable (tag)
+  ;;(message "complete variable: %s" tag)
+  (let* ((ttype (semantic-tag-type tag))
+	 type members)
+    (while (and ttype (listp ttype) (not (semantic-tag-type-members ttype)))
+      ;; find parent of variable
+      (setq type (semantic-analyze-find-tag (car ttype)))
+
+      (or (setq ttype (semantic-tag-get-attribute type :typedef))
+	  (setq ttype (semantic-tag-get-attribute type :class))))
+
+    (when (and ttype (listp ttype))
+      (setq type ttype))
+	
+    (setq members (semantic-tag-type-members type))
+    ;;(message "type %s" type)
+    (when members
+      ;;sort members with ca2+semantic
+      (when (boundp 'semantic-analyze-cache-tags)
+	(let* ((a (or
+		   ;; reuse context
+		   ca-source-semantic-context-cntxt
+		   ;; get current context
+		   (semantic-analyze-current-context (point))))
+	       ;; type that is expected in current context
+	       (desired-type (semantic-analyze-type-constraint a))
+	       ;; members of candidate sorted by reachability
+	       (members (if (not semantic-analyze-cache-tags)
+			    (semantic-analyze-possible-completions a)
+			  (ca-semantic-completions type desired-type))))))
+
+      (if (semantic-tag-get-attribute tag :pointer)
+	  (insert "->") (insert "."))
+	  
+      (mapcar '(lambda(tag) (cons (semantic-tag-name tag) tag)) 
+	      members))))
+
+(defun ca-source-semantic-continue (candidate)
+  (let ((tag (cdr-safe candidate)))
+    (when (semantic-tag-p tag)
+      (cond 
+       ((eq (semantic-tag-class tag) 'function)
+	(ca-source-semantic-continue-function tag))
+       ((eq (semantic-tag-class tag) 'variable)
+	(ca-source-semantic-continue-variable tag))))))
+
+
+
+
+
+;; semantic context completion
+(defvar ca-source-semantic-context-completions nil)
+(defvar ca-source-semantic-context-cntxt nil)
+
+(defun ca-source-semantic-context-decider-1 ()
+  (let* ((p (point))
+	 (a (semantic-analyze-current-context p))
+	 ;; XXX make this an option 
+	 ;; (syms (if a (semantic-ia-get-completions a p)))
+	 (syms (if a (semantic-analyze-possible-completions a)))
+	 (completions (mapcar 
+		       '(lambda(tag) (cons (semantic-tag-name tag) tag))
+		       syms)))
+    (when completions
+      (setq ca-source-semantic-context-cntxt a)
+      (setq ca-source-semantic-context-completions completions)
+      (or (car-safe (bounds-of-thing-at-point 'symbol))
+	  p))))
+
+;; decider works also with function argument templates, inserted 
+;; text needs to be deleted before semantic-analyze-current-context
+;; can do its work
+(defun ca-source-semantic-context-decider ()
+  (let* ((overlays (overlays-at (point)))
+	 (overlay (find-if '(lambda(ov) 
+			      (overlay-get ov 'yas/snippet)) 
+			   overlays)))
+    (if overlay
+	(let* ((start (overlay-start overlay))
+	       (end (overlay-end overlay))
+	       (reg (buffer-substring start end))
+	       (foo (delete-region start end))
+	       (prefix-start (ca-source-semantic-context-decider-1)))
+	  (if prefix-start
+	      prefix-start
+	    (insert reg)
+	    (goto-char start)
+	    nil))
+      (ca-source-semantic-context-decider-1))))
+
+(defun ca-source-semantic-context-candidates (prefix)
+  ca-source-semantic-context-completions)
+
+(defvar ca-source-semantic-context
+  '((decider . ca-source-semantic-context-decider)
+    (candidates . ca-source-semantic-context-candidates)
+    (info . ca-source-semantic-tag-summary)
+    (continue . ca-source-semantic-continue)
+    (sorted . t)
+    (name . "semantic-context"))
+  "ca2+ source for semantic context completion")
+
+
+
+
+
+;; semanticdb tags source - seems to be fastest
+(defun ca-source-semanticdb-tags-candidates (prefix)
+  (mapcar '(lambda (tag)
+	     (cons (semantic-tag-name tag) tag))
+	  (semanticdb-fast-strip-find-results 
+	   (semanticdb-find-tags-for-completion prefix))))
+
+(defvar ca-source-semanticdb-tags
+  '((candidates . ca-source-semanticdb-tags-candidates)
+    (continue . ca-source-semantic-continue)
+    (limit . 1)
+    (info . ca-source-semantic-tag-summary)
+    (name . "semanticdb-tags"))
+  "ca2+ semanticdb source for tag completion")
+
+
+
+
+
+;; standard semantic tags completion
 (defvar ca-source-semantic-tags-analysis nil)
 
 (defun ca-source-semantic-tags-decider ()
@@ -238,177 +458,39 @@
       (or (car-safe (bounds-of-thing-at-point 'symbol))
 	  (point)))))
 
-
 (defun ca-source-semantic-tags-candidates (prefix)
   ca-source-semantic-tags-analysis)
-
 
 (defun ca-source-semantic-tag-summary (candidate)
   (semantic-format-tag-summarize-with-file candidate nil t))
 
-
-;; modified from semantic-format.el, creates yas-template
-(defun ca-source-semantic-format-tag-arguments (args formatter)
-  "Format the argument list ARGS with FORMATTER.
-FORMATTER is a function used to format a tag.
-COLOR specifies if color should be used."
-  (let ((out nil)
-	(cnt 1))
-    (while args
-      (unless (equal (semantic-tag-name (car args)) "")
-	  (push (concat "${" (number-to-string cnt) ":"
-			(if (and formatter
-				 (semantic-tag-p (car args))
-				 (not (string= (semantic-tag-name (car args)) "")))
-			    (funcall formatter (car args) nil nil)
-			  (semantic-format-tag-name-from-anything
-			   (car args) nil nil 'variable))
-			"}")
-		out))
-      (setq cnt (1+ cnt))
-      (setq args (cdr args)))
-    ;;semantic-function-argument-separator
-    (mapconcat 'identity (nreverse out) ", ")))
-
-
-(defun ca-source-semantic-tags-action (candidate)
-  (let ((tag (cdr-safe candidate)))
-    (when (semantic-tag-p tag)
-      (cond 
-       ((eq (semantic-tag-class tag) 'function)
-	(yas/expand-snippet 
-	 (point) (point) 
-	 (concat 
-	  "(" (ca-source-semantic-format-tag-arguments
-	       (semantic-tag-function-arguments tag)
-	       #'semantic-format-tag-prototype) ")"
-	  (unless(looking-at "[[:space:]]*\\()\\|,\\)") ";"))))
-       ;; expansion was called for a complete variable name
-       ((and (not (memq this-command (cdr ca-continue-commands)))
-	     (eq (semantic-tag-class tag) 'variable))
-	(let ((candidates (ca-source-semantic-continue candidate)))
-	  (when candidates
-	    (message "got cands")
-	    (ca-begin candidates ca-source-semantic-context))))))))
-
-
 (defvar ca-source-semantic-tags
   '((decider . ca-source-semantic-tags-decider)
     (candidates . ca-source-semantic-tags-candidates)
-    (action . ca-source-semantic-tags-action)
+    (continue . ca-source-semantic-continue)
     (limit . 1)
     (info . ca-source-semantic-tag-summary)
-    (filter . t) ;; source provides all candidates: filter with prefix
     (name . "semantic-tags"))
   "ca2+ semantic source for tag completion")
-
-
-;; semantic context completion
-(defvar ca-source-semantic-context-completions nil)
-(defvar ca-source-semantic-context-cntxt nil)
-
-
-(defun ca-source-semantic-context-decider ()
-  (let* ((p (point))
-	 (a (semantic-analyze-current-context p))
-	 ;; XXX make this an option 
-	 ;; (syms (if a (semantic-ia-get-completions a p)))
-	 (syms (if a (semantic-analyze-possible-completions a)))
-	 (completions (mapcar 
-		       '(lambda(tag) (cons (semantic-tag-name tag) tag))
-		       syms)))
-    (when completions
-      (setq ca-source-semantic-context-cntxt a)
-      (setq ca-source-semantic-context-completions completions)
-      (or (car-safe (bounds-of-thing-at-point 'symbol))
-	  p))))
-
-
-(defun ca-source-semantic-continue (candidate)
-  (let ((tag (cdr-safe candidate)))
-    (when (and tag (eq (semantic-tag-class tag) 'variable))
-      (let* ((ttype (semantic-tag-type tag))
-	      type members)
-
-	(while (and ttype (listp ttype) (not (semantic-tag-type-members ttype)))
-	  ;; find parent of variable
-	  (setq type (semantic-analyze-find-tag (car ttype)))
-	  ;; check wheter the parent is typedef, TODO class, whatever
-	  ;;(or 
-	  (setq ttype (semantic-tag-get-attribute type :typedef)))
-	  ;;   (setq ttype (semantic-tag-get-attribute type :class))))
-
-	(unless (setq members (semantic-tag-type-members ttype))
-	  (setq members (semantic-tag-type-members type)))
-
-	(when members
-	  ;; sort members by reachability
-	  (if (and (boundp 'semantic-analyze-cache-tags)
-		   semantic-analyze-cache-tags 
-		   ca-source-semantic-context-cntxt)
-	      (let* ((a ca-source-semantic-context-cntxt)
-		    (desired-type (semantic-analyze-type-constraint a)))
-		(setq members (ca-semantic-completions
-			       (or ttype type)
-			       desired-type))))
-
-	  (if (semantic-tag-get-attribute tag :pointer)
-	      (insert "->") (insert "."))
-	    
-	  (mapcar '(lambda(tag) (cons (semantic-tag-name tag) tag)) 
-		  members))))))
-
-
-(defun ca-source-semantic-context-candidates (prefix)
-  ca-source-semantic-context-completions)
-
-
-(defvar ca-source-semantic-context
-  '((decider . ca-source-semantic-context-decider)
-    (candidates . ca-source-semantic-context-candidates)
-    (action . ca-source-semantic-tags-action)
-    (info . ca-source-semantic-tag-summary)
-    (continue . ca-source-semantic-continue)
-    (sorted . t)
-    (trigger . ".")
-    ;;(filter . t)
-    (name . "semantic-context"))
-  "ca2+ source for semantic context completion")
-
-
-;; semantic completion for function argument templates, inserted 
-;; text needs to be deleted before semantic-analyze-current-context
-;; can do its work
-(defun ca-source-semantic-args-decider ()
-  (let* ((overlays (overlays-at (point)))
-	 (overlay (find-if '(lambda(ov) 
-			      (overlay-get ov 'yas/snippet)) 
-			   overlays)))
-    (when overlay
-      (let* ((start (overlay-start overlay))
-	    (end (overlay-end overlay))
-	    (reg (buffer-substring start end))
-	    (foo (delete-region start end))
-	    (prefix-start (ca-source-semantic-context-decider)))
-	(if prefix-start
-	    prefix-start
-	  (insert reg)
-	  (goto-char start)
-	  nil)))))
-
-
-(defvar ca-source-semantic-yas-arguments
-  '((decider . ca-source-semantic-args-decider)
-    (candidates . ca-source-semantic-context-candidates)
-    (info . ca-source-semantic-tag-summary)
-    (continue . ca-source-semantic-continue)
-    (sorted . t)
-    ;;(sort-by-occurrence . t)
-    (name . "semantic-arguments"))
-  "ca2+ source for semantic argument completion")
-
 
 
 
 (provide 'ca2+sources)
 
+
+
+
+
+
+
+;; (defvar ca-source-semantic-yas-arguments
+;;   '((decider . ca-source-semantic-args-decider)
+;;     (candidates . ca-source-semantic-context-candidates)
+;;     (action . ca-source-semantic-action)
+;;     (info . ca-source-semantic-tag-summary)
+;;     (continue . ca-source-semantic-continue)
+;;     (sorted . t)
+;;     (limit . 1)
+;;     ;;(sort-by-occurrence . t)
+;;     (name . "semantic-arguments"))
+;;   "ca2+ source for semantic argument completion")
