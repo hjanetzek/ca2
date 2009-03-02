77c2a4afcd2f3cf8a66ddcbf90294dc1a62f16b6
hannes <jeff@x40.(none)>
2009-03-03 00:40:54 +0100

    docs

--- ca2+init.el
+++ ca2+init.el
@@ -2,7 +2,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; install sources ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'ca2+)
 (require 'ca2+sources)
-
+(require 'ca2+source-semantic)
+(require 'ca2+ac)
 
 ;; Options what tab should preferably do.
 ;; just uncomment the preffered method:
@@ -29,95 +30,90 @@
 
 (ca-clear-completion-sources)
 
-;; mode sources
+;;;; MODE SPECIFIC sources
 ;; sources are pushed on the list: load lower priority sources first
 
-;; complete elisp symbols
-(ca-add-completion-source ca-source-lisp
-			  '(emacs-lisp-mode 
-			    lisp-interaction-mode))
+;;;; ELISP source:
+(ca-add-source ca-source-lisp
+	       '(emacs-lisp-mode 
+		 lisp-interaction-mode))
 
-;; (ca-add-completion-source ca-source-semanticdb-tags
-;; 			  '(emacs-lisp-mode 
-;; 			    lisp-interaction-mode))
-;; GTAGS source:
+;;;; GTAGS source:
 ;; complete prefixes with tags found in gtags tags table
 (eval-after-load 'gtags
   '(progn 
-     (ca-add-completion-source ca-source-gtags
-			       '(c++-mode c-mode java-mode))))
+     (ca-add-source ca-source-gtags
+		    '(c++-mode c-mode java-mode))))
 
 
-;; YASNIPPET source:
+
+;;;; SEMANTIC source:
+(eval-after-load 'semantic
+  '(progn 
+     (require 'semantic-ia)
+     ;; Enable experimental, but faster version.  You need to run
+     ;; ca-semantic-clear-cache to update cached (non local) tags
+     ;; tables atm, i.e. when a new type or function was added.
+     ;; Search results for a desired type are also sorted by
+     ;; reachability so vars having the desired type come first, then
+     ;; vars that have members from which desired type is reachable.
+     (require 'ca2+semantic)
+
+     ;;;; complete prefix with tags found in semantics tags table
+     ;; (ca-add-completion-source ca-source-semantic-tags
+     ;; 				'(c++-mode c-mode))
+
+     ;;;; this source tries to figure out from context what preferred
+     ;; candidates are. e.g: for 'int bla =' it finds vars and
+     ;; functions that have int as type, same within function
+     ;; arguments. it also sorts candidates first that have members
+     ;; from which the desired type is reachable (when using
+     ;; ca2+semantic).
+     (ca-add-source ca-source-semantic-context
+		    '(c++-mode c-mode))
+
+     (ca-add-source ca-source-semanticdb-tags
+		    '(c++-mode c-mode))
+     
+     ;;;; use dabbrev to get completion fast, but allow to C-ret 
+     ;; trigger semantic action for selected candidate
+     ;; (ca-add-completion-source ca-source-semantic-with-dabbrev
+     ;;  			       '(c++-mode c-mode))
+     
+     ;;;; OMNICOMPLETION:
+     ;; uncomment things below for omnicompletion. though you can just
+     ;; type [tab] and C-ret to insert '.' or respectively '->'
+     ;; automatically and start completion menu.
+     ;;
+     ;; (defun ca-semantic-completion (arg)
+     ;;   (interactive "p")
+     ;;   (self-insert-command arg)
+     ;;   (when (and (= arg 1))
+     ;; 	 (ca-begin nil ca-source-semantic-context)))
+     ;; (defun ca-semantic-c-hook ()
+     ;;   (local-set-key "." 'ca-semantic-completion)
+     ;;   (local-set-key ">" 'ca-semantic-completion))
+     ;; (add-hook 'c-mode-common-hook 'ca-semantic-c-hook)
+     ))
+
+
+;;;; GENERAL sources are tried after mode specific ones 
+(ca-add-source ca-source-filename 'otherwise)
+(ca-add-source ca-source-dabbrev 'otherwise)
+
+;;;; YASNIPPET source:
+;; this source show possible completions when a prefix
+;; matches more than one yasnippet template
+
 ;; it seems that this needs to be set before '(require 'yasnippet)'
-;; change this to your liking, but tab would interfer with 
+;; change this to your liking, but tab would interfere with 
 ;; completion within yas templates.
 (defvar yas/next-field-key (kbd "C-f"))
 (defvar yas/prev-field-key (kbd "C-b"))
 
 (eval-after-load 'yasnippet
   '(progn
-     ;; this source show possible completions when a prefix
-     ;; matches more than one yasnippet template
-     (ca-add-completion-source ca-source-yasnippet
-			       '(emacs-lisp-mode 
-				 lisp-interaction-mode
-				 c++-mode c-mode java-mode 
-				 'otherwise))))
-
-;; SEMANTIC source:
-(eval-after-load 'semantic
-  '(progn 
-     (require 'semantic-ia)
-     ;; experimental, but faster version. 
-     ;; you need to run ca-semantic-clear-cache 
-     ;; to update cached tags tables
-     (require 'ca2+semantic)
-
-     ;; just complete prefixes with tags found in semantics
-     ;; tags table
-     ;; (ca-add-completion-source ca-source-semantic-tags
-     ;; 				'(c++-mode c-mode))
-
-     ;; this source tries to figure out from context what 
-     ;; preferred candidates are. e.g: for 'int bla =' it 
-     ;; finds vars and functions that have int as type, 
-     ;; same within function arguments. it also sorts candidates
-     ;; first that have members from which the desired 
-     ;; type is reachable (when using ca2+semantic).
-     (ca-add-completion-source ca-source-semantic-context
-     			       '(c++-mode c-mode))
-
-     ;; use dabbrev as first completion method
-     ;; (ca-add-completion-source ca-source-dabbrev
-     ;; 			       '(c++-mode c-mode java-mode))
-
-     (ca-add-completion-source ca-source-semanticdb-tags
-      			       '(c++-mode c-mode))
-
-
-     ;; (defun ca-semantic-completion (arg)
-     ;;   (interactive "p")
-     ;;   (self-insert-command arg)
-     ;;   (when (and (= arg 1))
-     ;; 	 (ca-begin nil ca-source-semantic-context)))
-
-     ;; (defun ca-semantic-c-hook ()
-     ;;   (local-set-key "." 'ca-semantic-completion)
-     ;;   (local-set-key ">" 'ca-semantic-completion))
-
-     ;; (add-hook 'c-mode-common-hook 'ca-semantic-c-hook)
-     ))
-
-
-
-
-;; general sources are tried after mode specific ones 
-(ca-add-completion-source ca-source-filename
-			  'otherwise)
-
-(ca-add-completion-source ca-source-dabbrev
-			  'otherwise)
+     (ca-add-source ca-source-yasnippet 'otherwise)))
 
 
 
